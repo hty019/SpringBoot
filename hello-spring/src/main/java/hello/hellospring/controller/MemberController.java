607d005f75e649577e7052e162d60b39fe7e4df2
hty019 <hty019@gmail.com>
2021-05-14 21:33:11 +0900

    회원 조회 기능 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,7 +16,7 @@ public class MemberController {
 
     @Autowired
     public MemberController(MemberService memberService) {
-        this.memberService=memberService;
+        this.memberService = memberService;
     }
 
     @GetMapping("/members/new")
@@ -27,5 +30,12 @@ public class MemberController {
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
